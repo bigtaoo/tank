a55adc04dafe5c7fa060ea539cee60a9e2f660cb
TaoWang <48122403+bigtaoo@users.noreply.github.com>
2024-12-08 18:08:04 +0100

    adjust stop position

--- Unity-Tank/Assets/Scripts/Hotfix/Client/Tank/TankPlayerSystem.cs
+++ Unity-Tank/Assets/Scripts/Hotfix/Client/Tank/TankPlayerSystem.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ET.Client
 {
     [EntitySystemOf(typeof(TankPlayerComponent))]
@@ -15,50 +17,8 @@ namespace ET.Client
         [EntitySystem]
         private static void Update(this TankPlayerComponent self)
         {
-            var currentTime = TimeInfo.Instance.ClientFrameTime();
-            var deltaTime = currentTime - self.LastFrameTime;
-            self.LastFrameTime = currentTime;
-            var distance = self.MoveSpeed * deltaTime / 1000;
-            var position = self.Position;
-
-            switch(self.MoveDirection)
-            {               
-                case TankDirection.Left:
-                {
-                    position.X -= distance;
-                    self.Rotation = 90;
-                    break;
-                }
-                case TankDirection.Right:
-                {
-                    position.X += distance;
-                    self.Rotation = 270;
-                    break;
-                }
-                case TankDirection.Up:
-                {
-                    position.Y += distance;
-                    self.Rotation = 0;
-                    break;
-                }
-                case TankDirection.Down:
-                {
-                    position.Y -= distance;
-                    self.Rotation = 180;
-                    break;
-                }
-                case TankDirection.None:
-                default:
-                {
-                    return;
-                }
-            }
-
-            var mapTilesComponent = self.Root().GetComponent<TankMapTilesComponent>();
-            if (mapTilesComponent.IsInMap(position) && mapTilesComponent.GetTile(position) == null)
-            {
-                self.Position = position;
-            }
+            self.UpdatePosition();
+            self.AdjustStopPosition();
         }
 
         public static void InilializePlayer(this TankPlayerComponent self, TankPosition spawnPosition)
@@ -72,6 +32,81 @@ namespace ET.Client
         public static void SetMoveDirection(this TankPlayerComponent self, TankDirection moveDirection)
         {
             self.MoveDirection = moveDirection;
+        }
+
+        private static void UpdatePosition(this TankPlayerComponent self)
+        {
+            var currentTime = TimeInfo.Instance.ClientFrameTime();
+            var deltaTime = currentTime - self.LastFrameTime;
+            self.LastFrameTime = currentTime;
+            var distance = self.MoveSpeed * deltaTime / 1000;
+            var position = self.Position;
+
+            switch (self.MoveDirection)
+            {
+                case TankDirection.Left:
+                    {
+                        position.X -= distance;
+                        self.Rotation = 90;
+                        break;
+                    }
+                case TankDirection.Right:
+                    {
+                        position.X += distance;
+                        self.Rotation = 270;
+                        break;
+                    }
+                case TankDirection.Up:
+                    {
+                        position.Y += distance;
+                        self.Rotation = 0;
+                        break;
+                    }
+                case TankDirection.Down:
+                    {
+                        position.Y -= distance;
+                        self.Rotation = 180;
+                        break;
+                    }
+                case TankDirection.None:
+                default:
+                    {
+                        return;
+                    }
+            }
+
+            var mapTilesComponent = self.Root().GetComponent<TankMapTilesComponent>();
+            if (mapTilesComponent.IsInMap(position) && mapTilesComponent.GetTile(position) == null)
+            {
+                self.Position = position;
+            }
+        }
+
+        private static void AdjustStopPosition(this TankPlayerComponent self)
+        {
+            if (self.MoveDirection != TankDirection.None)
+            {
+                return;
+            }
+            if (self.CurrentDirection == TankDirection.Up || self.CurrentDirection == TankDirection.Down)
+            {
+                var position = self.Position;
+                position.Y = (float)Math.Round(self.Position.Y);
+                Log.Warning($"Ajust Y: current Y: {self.Position.Y}, target Y: {position.Y}");
+                if (position.Y != self.Position.Y)
+                {
+                    self.Position = position;
+                }
+            }
+            else if (self.CurrentDirection == TankDirection.Left || self.CurrentDirection == TankDirection.Right)
+            {
+                var position = self.Position;
+                position.X = (float)Math.Round(self.Position.X);
+                if ( position.X != self.Position.X)
+                {
+                    self.Position = position;
+                }
+            }
         }
     }
 }
