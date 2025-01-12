9d7a57900dfca6df8a3c4febbfc775e9070c6249
TaoWang <48122403+bigtaoo@users.noreply.github.com>
2025-01-12 20:44:05 +0100

    0

--- Unity-Tank/Assets/Scripts/Hotfix/Client/Tank/Helper/TankMovementHelper.cs
+++ Unity-Tank/Assets/Scripts/Hotfix/Client/Tank/Helper/TankMovementHelper.cs
@@ -5,32 +5,32 @@ namespace ET
 {
     public static class TankMovementHelper
     {
-        public static (TankPosition, int) Move(TankPosition position, TankDirection direction, float distance)
+        public static (TankPosition, int) Move(TankPosition currentPosition, TankDirection direction, float distance)
         {
             var rotation = 0;
             switch (direction)
             {
                 case TankDirection.Left:
                     {
-                        position.X -= distance;
+                        currentPosition.X -= distance;
                         rotation = 90;
                         break;
                     }
                 case TankDirection.Right:
                     {
-                        position.X += distance;
+                        currentPosition.X += distance;
                         rotation = 270;
                         break;
                     }
                 case TankDirection.Up:
                     {
-                        position.Y += distance;
+                        currentPosition.Y += distance;
                         rotation = 0;
                         break;
                     }
                 case TankDirection.Down:
                     {
-                        position.Y -= distance;
+                        currentPosition.Y -= distance;
                         rotation = 180;
                         break;
                     }
@@ -40,41 +40,41 @@ namespace ET
                         break;
                     }
             }
-            return (position, rotation);
+            return (currentPosition, rotation);
         }
 
-        public static bool CanTankMoveToPosition(Scene root, TankPosition position, TankDirection direction)
+        public static bool CanTankMoveToPosition(Scene root, TankPosition targetPosition, TankDirection direction)
         {
             var mapTilesComponent = root.GetComponent<TankMapTilesComponent>();
-            if (!mapTilesComponent.IsInMap(position, 1.0f))
+            if (!mapTilesComponent.IsInMap(targetPosition, 1.0f))
             {
                 return false;
             }
-            position.X = PositionToTile(direction, position.X);
-            position.Y = PositionToTile(direction, position.Y);
+            targetPosition.X = PositionToTile(direction, targetPosition.X);
+            targetPosition.Y = PositionToTile(direction, targetPosition.Y);
 
             switch (direction)
             {
                 case TankDirection.Left:
                     //Log.Warning($"Move left: x {position.X} y:{position.Y} up {mapTilesComponent.GetTile(new TankPosition { X = position.X - 1, Y = position.Y })}," +
                     //    $"down {mapTilesComponent.GetTile(new TankPosition { X = position.X - 1, Y = position.Y - 1 })}");
-                    return mapTilesComponent.GetTile(new TankPosition { X = position.X - 1, Y = position.Y }) == null &&
-                        mapTilesComponent.GetTile(new TankPosition { X = position.X - 1, Y = position.Y - 1 }) == null;
+                    return mapTilesComponent.GetTile(new TankPosition { X = targetPosition.X - 1, Y = targetPosition.Y }) == null &&
+                        mapTilesComponent.GetTile(new TankPosition { X = targetPosition.X - 1, Y = targetPosition.Y - 1 }) == null;
                 case TankDirection.Right:
                     //Log.Warning($"Move Right: x {position.X} y:{position.Y} up {mapTilesComponent.GetTile(new TankPosition { X = position.X, Y = position.Y })}," +
                     //    $"down {mapTilesComponent.GetTile(new TankPosition { X = position.X, Y = position.Y - 1 })}");
-                    return mapTilesComponent.GetTile(new TankPosition { X = position.X, Y = position.Y }) == null &&
-                    mapTilesComponent.GetTile(new TankPosition { X = position.X, Y = position.Y - 1 }) == null;
+                    return mapTilesComponent.GetTile(new TankPosition { X = targetPosition.X, Y = targetPosition.Y }) == null &&
+                    mapTilesComponent.GetTile(new TankPosition { X = targetPosition.X, Y = targetPosition.Y - 1 }) == null;
                 case TankDirection.Up:
                     //Log.Warning($"Move Up: x {position.X} y:{position.Y} right {mapTilesComponent.GetTile(new TankPosition { X = position.X, Y = position.Y })}," +
                     //    $"left {mapTilesComponent.GetTile(new TankPosition { X = position.X - 1, Y = position.Y })}");
-                    return mapTilesComponent.GetTile(new TankPosition { X = position.X, Y = position.Y }) == null &&
-                        mapTilesComponent.GetTile(new TankPosition { X = position.X - 1, Y = position.Y }) == null;
+                    return mapTilesComponent.GetTile(new TankPosition { X = targetPosition.X, Y = targetPosition.Y }) == null &&
+                        mapTilesComponent.GetTile(new TankPosition { X = targetPosition.X - 1, Y = targetPosition.Y }) == null;
                 case TankDirection.Down:
                     //Log.Warning($"Move Down: x {position.X} y:{position.Y} right {mapTilesComponent.GetTile(new TankPosition { X = position.X, Y = position.Y - 1 })}," +
                     //    $"left {mapTilesComponent.GetTile(new TankPosition { X = position.X - 1, Y = position.Y - 1 })}");
-                    return mapTilesComponent.GetTile(new TankPosition { X = position.X, Y = position.Y - 1 }) == null &&
-                        mapTilesComponent.GetTile(new TankPosition { X = position.X - 1, Y = position.Y - 1 }) == null;
+                    return mapTilesComponent.GetTile(new TankPosition { X = targetPosition.X, Y = targetPosition.Y - 1 }) == null &&
+                        mapTilesComponent.GetTile(new TankPosition { X = targetPosition.X - 1, Y = targetPosition.Y - 1 }) == null;
                 case TankDirection.None:
                 default:
                     return false;
