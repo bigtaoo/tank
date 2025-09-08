8be3c3c3380e49dbe66528c58d5b52561ce3e836
王涛 <wangtao@wangtaodeMBP.fritz.box>
2025-09-08 23:30:05 +0200

    locale bind script

---
+++ Unity-Tank/Assets/Scripts/Core/Helper/LocalizeTMPBinder.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.Localization.Components;
+
+[RequireComponent(typeof(TMP_Text))]
+[RequireComponent(typeof(LocalizeStringEvent))]
+public class LocalizeTMPBinder : MonoBehaviour
+{
+    void Reset() => Bind();
+    void Awake() => Bind();
+
+    void Bind()
+    {
+        var tmp = GetComponent<TMP_Text>();
+        var ev  = GetComponent<LocalizeStringEvent>();
+        if (ev != null && tmp != null)
+        {
+            ev.OnUpdateString.RemoveAllListeners();
+            ev.OnUpdateString.AddListener(tmp.SetText);
+            ev.RefreshString();
+        }
+    }
+}
